dd3145861f6f1d3ab47864de0441ae565d89c16c
Abhishek Deshmukh <manishp@301io.com>
2024-12-01 21:57:23 +0530

    slug removed

--- app/blog/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function BlogPost({ params }: { params: { slug: string } }) {
-  return (
-    <main>
-      <h1>{params.slug.replace("-", " ").toUpperCase()}</h1>
-      <p>This is a dynamic blog post page.</p>
-    </main>
-  );
-}
