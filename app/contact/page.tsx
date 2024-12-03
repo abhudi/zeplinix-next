9cc9c07e659aacda525db43551d5e6c03e672a90
Abhishek Deshmukh <manishp@301io.com>
2024-12-03 23:08:18 +0530

    contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,8 @@
+import GetInTouchForm from "../components/contactCard";
 export default function Contact() {
   return (
-    <main>
-      <h1>Contact Us</h1>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
-
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-
-        <button type="submit">Send</button>
-      </form>
-    </main>
+    <div>
+      <GetInTouchForm />
+    </div>
   );
 }
